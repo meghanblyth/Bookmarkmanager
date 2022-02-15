c424055ac28e2e2baefe05478e6b3fd424cd1d21
meghanblyth <megblyth@gmail.com>
2022-02-15 17:59:20 +0000

    Step 7, interacting with PS from Ruby

--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -2,11 +2,11 @@ require 'bookmark'
 
 describe Bookmark do
   describe '.all' do
-    it 'returns all of the bookmarks' do
-      bookmarks = Bookmark.all
-      expect(bookmarks).to include('http://www.makersacademy.com')
-      expect(bookmarks).to include('http://www.destroyallsoftware.com')
-      expect(bookmarks).to include('http://www.google.com')
+    it 'returns a list of bookmarks' do
+      bookmarks = Bookmark.all      
+      expect(bookmarks).to include "http://www.makersacademy.com/"
+      expect(bookmarks).to include "http://www.destroyallsoftware.com"
+      expect(bookmarks).to include "http://www.google.com/"
     end
   end
 end
